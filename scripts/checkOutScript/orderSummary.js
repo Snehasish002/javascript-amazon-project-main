3b8d2e9a79a06767365233f4c970d2194bdf4224
Snehasish <snehasishmohanty9439@gmail.com>
2024-05-08 23:26:02 +0530

    Make payment summary interactive

--- scripts/checkOutScript/orderSummary.js
+++ scripts/checkOutScript/orderSummary.js
@@ -4,9 +4,9 @@ import { formatCurrency } from "../utils/money.js";
 import {hello} from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { renderPaymentummary } from "./paymentSummery.js";
 
 
-hello();
 
 
 
@@ -152,6 +152,8 @@ export function renderOrderSummery(){
                 );
 
                 container.remove();
+
+                renderPaymentummary();
             });
         });
 
@@ -162,6 +164,7 @@ export function renderOrderSummery(){
                 const {productId, deliveryOptionId} = element.dataset;
                 updateDeliveryOption(productId,deliveryOptionId);
                 renderOrderSummery();
+                renderPaymentummary();
                 
             });
         });
