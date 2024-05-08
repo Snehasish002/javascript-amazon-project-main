8cae33c27614ddd9ae0a4c72080593c512f82fa8
Snehasish <snehasishmohanty9439@gmail.com>
2024-05-08 23:17:26 +0530

    Save data and generate HTML for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,5 @@
 import { renderOrderSummery } from "./checkOutScript/orderSummary.js";
+import { renderPaymentummary } from "./checkOutScript/paymentSummery.js";
 
+renderPaymentummary();
 renderOrderSummery();
