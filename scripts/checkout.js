8d32c70792cf5cf44adb35e1ac53cf93500ec013
Snehasish <snehasishmohanty9439@gmail.com>
2024-05-13 23:49:56 +0530

    Creat a Oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummery } from "./checkOutScript/orderSummary.js";
 import { renderPaymentummary } from "./checkOutScript/paymentSummery.js";
+import '../data/cart-Oop.js';
 
 renderPaymentummary();
 renderOrderSummery();
