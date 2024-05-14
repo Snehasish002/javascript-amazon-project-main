70264aa4461bf79be115c27936e6f5a413e50a73
Snehasish <snehasishmohanty9439@gmail.com>
2024-05-14 20:59:09 +0530

    Converts Products Into Classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummery } from "./checkOutScript/orderSummary.js";
 import { renderPaymentummary } from "./checkOutScript/paymentSummery.js";
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 
 renderPaymentummary();
 renderOrderSummery();
