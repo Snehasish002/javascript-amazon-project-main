a927227737ee49c9d68f55e8106f28a74841dfe8
Snehasish <snehasishmohanty9439@gmail.com>
2024-05-16 12:33:46 +0530

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,5 +2,7 @@ import { renderOrderSummery } from "./checkOutScript/orderSummary.js";
 import { renderPaymentummary } from "./checkOutScript/paymentSummery.js";
 //import '../data/cart-class.js';
 
+import '../data/backend-practice.js';
+
 renderPaymentummary();
 renderOrderSummery();
