80aefa8917e03802dbd9bf7ac0bb2271ede38d9c
Snehasish <snehasishmohanty9439@gmail.com>
2024-05-16 23:01:02 +0530

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,12 @@
 import { renderOrderSummery } from "./checkOutScript/orderSummary.js";
 import { renderPaymentummary } from "./checkOutScript/paymentSummery.js";
+import { loadProducts } from "../data/products.js";
 //import '../data/cart-class.js';
 
-import '../data/backend-practice.js';
+//import '../data/backend-practice.js';
 
-renderPaymentummary();
-renderOrderSummery();
+
+loadProducts( () => {
+    renderPaymentummary();
+    renderOrderSummery();
+});
